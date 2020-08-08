78a2e6639ea73edd62a3463c38f892e9c282570e
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-08-08 10:53:12 -0300

    Tab navigator styles

---
+++ src/pages/Favorites/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { View, Image, Text, TouchableOpacity , ImageBackground} from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
+
+import teachImg from '../../assets/images/give-classes-background.png'
+
+import styles from './styles'
+
+const Favorites = () => {
+  const { goBack } = useNavigation()
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={teachImg}
+        style={styles.imgBg}
+        resizeMode="contain"
+      >
+        <Text style={styles.title}>Do you want to be a Proffy?</Text>
+        <Text style={styles.description}>
+          To get started, you need to register a teacher account on the web platform.
+        </Text>
+      </ImageBackground>
+
+      <RectButton
+        onPress={goBack}
+        style={styles.okButton}
+      >
+        <Text style={styles.okButtonText}>Register</Text>
+      </RectButton>
+    </View>
+  )
+}
+
+export default Favorites
--- src/routes/StudyTabs.js
+++ src/routes/StudyTabs.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Platform } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 
 import TeacherList from '../pages/TeacherList'
 import Favorites from '../pages/Favorites'
@@ -7,9 +9,58 @@ import Favorites from '../pages/Favorites'
 const { Navigator, Screen } = createBottomTabNavigator()
 
 const StudyTabs = () => (
-  <Navigator>
-    <Screen name="TeacherList" component={TeacherList} />
-    <Screen name="Favorites" component={Favorites} />
+  <Navigator
+    tabBarOptions={{
+      style: {
+        elevation: 0,
+        shadowOpacity: 0,
+        height: Platform.OS === 'ios' ? 84 : 64
+      },
+      tabStyle: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingBottom: Platform.OS === 'ios' ? 20 : 0
+      },
+      safeAreaInsets: {
+        bottom: 0,
+      },
+      iconStyle: {
+        flex: 0,
+        width: 20,
+        height: Platform.OS === 'ios' ? 24 : 20
+      },
+      labelStyle: {
+        fontFamily: 'Archivo_700Bold',
+        fontSize: 13,
+        marginLeft: 16
+      },
+      inactiveBackgroundColor: '#fafafc',
+      activeBackgroundColor: '#ebebf5',
+      inactiveTintColor: '#c1bccc',
+      activeTintColor: '#32264d'
+    }}
+  >
+    <Screen
+      name="TeacherList"
+      component={TeacherList}
+      options={{
+        tabBarLabel: 'Proffys',
+        tabBarIcon: ({ size, color }) => (
+          <Ionicons name="ios-easel" size={size} color={color} />
+        )
+      }}
+    />
+    <Screen
+      name="Favorites"
+      component={Favorites}
+      options={{
+        tabBarLabel: 'Favorites',
+        tabBarIcon: ({ size, color }) => (
+          <Ionicons name="ios-heart" size={size} color={color} />
+        )
+      }}
+    />
   </Navigator>
 )
 
