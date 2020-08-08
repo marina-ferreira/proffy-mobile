dd569a922cef786cc1a4e8fa54aecfcb6e8ff6b0
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-08-07 23:14:20 -0300

    Landing page

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
-import { View, Image, Text } from 'react-native'
+import { View, Image, Text, TouchableOpacity } from 'react-native'
 
 import landingImg from '../../assets/images/landing.png'
+import studyIcon from '../../assets/images/icons/study.png'
+import teachIcon from '../../assets/images/icons/give-classes.png'
+import heartIcon from '../../assets/images/icons/heart.png'
+
 import styles from './styles'
 
 const Landing = () => {
@@ -12,6 +16,23 @@ const Landing = () => {
       <Text style={styles.title}>
         Welcome {'\n'}
         <Text style={styles.titleBold}>What do you want to do?</Text>
+      </Text>
+
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+          <Image source={studyIcon} />
+          <Text style={styles.buttonText}>Study</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+          <Image source={teachIcon} />
+          <Text style={styles.buttonText}>Teach</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={styles.totalConnections}>
+        A total of 200 connections achieved {' '}
+        <Image source={heartIcon} />
       </Text>
     </View>
   )
--- src/pages/Landing/styles.js
+++ src/pages/Landing/styles.js
@@ -14,14 +14,52 @@ const styles = StyleSheet.create({
   },
 
   title: {
-    color: '#fafafa',
+    fontFamily: 'Poppins_400Regular',
     fontSize: 20,
+    color: '#fafafa',
     lineHeight: 30,
     marginTop: 80
   },
 
   titleBold: {
-    fontWeight: 'bold'
+    fontFamily: 'Poppins_600SemiBold',
+  },
+
+  buttonsContainer: {
+    flexDirection: 'row',
+    marginTop: 40,
+    justifyContent: 'space-between'
+  },
+
+  button: {
+    width: '48%',
+    height: 150,
+    borderRadius: 8,
+    padding: 24,
+    justifyContent: 'space-between'
+  },
+
+  buttonPrimary: {
+    backgroundColor: '#9871f5'
+  },
+
+  buttonSecondary: {
+    backgroundColor: '#04d361'
+  },
+
+  buttonText: {
+    fontFamily: 'Archivo_700Bold',
+    color: '#fff',
+    fontSize: 20
+  },
+
+  totalConnections: {
+    maxWidth: 140,
+    marginTop:40,
+    fontFamily: 'Poppins_400Regular',
+    color: '#d4c2ff',
+    fontSize: 12,
+    lineHeight: 20,
   }
 })
 
